b2f19c0cddef4f7f7f591c2768fe704b29c13753
Sandro Arpi <sandro.teste72@gmail.com>
2021-02-04 01:59:05 -0300

    Final work first commit

---
+++ backend/src/main/java/com/sandroarpi/bdsfinalwork/BdsfinalworkApplication.java
@@ -0,0 +1,13 @@
+package com.sandroarpi.bdsfinalwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdsfinalworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdsfinalworkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sandroarpi/bdsfinalwork/BdsfinalworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandroarpi.bdsfinalwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdsfinalworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
