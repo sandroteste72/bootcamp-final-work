e5ede522098c58cacdb65465140ea114e479e226
Sandro Arpi <sandro.teste72@gmail.com>
2021-02-05 20:25:06 -0300

    Find Client by id, exception handler

--- backend/src/main/java/com/sandroarpi/bdsfinalwork/services/ClientService.java
+++ backend/src/main/java/com/sandroarpi/bdsfinalwork/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.sandroarpi.bdsfinalwork.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.sandroarpi.bdsfinalwork.dtos.ClientDTO;
 import com.sandroarpi.bdsfinalwork.entities.Client;
 import com.sandroarpi.bdsfinalwork.repositories.ClientRepository;
+import com.sandroarpi.bdsfinalwork.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -21,5 +23,12 @@ public class ClientService {
 	public List<ClientDTO> findAll(){
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 }
